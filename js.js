c48178beab87328e623c81bba9ad4d1255f6bba2
Robert <wyb4wiciel@gmail.com>
2020-11-12 21:02:38 +0100

    left side temporaly done

--- js.js
+++ js.js
@@ -2,26 +2,27 @@
 
 const navLine = [...document.querySelectorAll(".line")];
 const navColor = [...document.querySelectorAll(".nav__text--link")];
-const about = document.querySelector(".about");
-const projects = document.querySelector(".projects");
-const contact = document.querySelector(".contact");
+const subpages = [...document.querySelectorAll(".subpages")];
 
 // NAV SCROLL LISTENER
 window.addEventListener("scroll", () => {
-  let heightY = window.scrollY;
-  console.log(contact.offsetTop);
+  for (let [subpage, className] of subpages.entries()) {
+    let { y: hY } = className.getBoundingClientRect();
+    hY <= 200 && hY >= 0 ? navChanges(subpage) : null;
+  }
 });
 // NAV CLICK LISTENER
-for (let [i] of navColor.entries()) {
-  navColor[i].addEventListener("click", function () {
-    navChanges(i);
+for (let [nav] of navColor.entries()) {
+  navColor[nav].addEventListener("click", function () {
+    navChanges(nav);
   });
 }
-function navChanges(i) {
-  for (let [i] of navColor.entries()) {
-    navColor[i].classList.remove("active__color");
-    navLine[i].classList.remove("active__line");
+//NAV  COLOR CHANGER
+function navChanges(navData) {
+  for (let [navData] of navColor.entries()) {
+    navColor[navData].classList.remove("active__color");
+    navLine[navData].classList.remove("active__line");
   }
-  navColor[i].classList.add("active__color");
-  navLine[i].classList.add("active__line");
+  navColor[navData].classList.add("active__color");
+  navLine[navData].classList.add("active__line");
 }
