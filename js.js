e516a870bfd22de04e39e278565ec9d010eb3b5f
Robert <wyb4wiciel@gmail.com>
2020-11-15 14:47:28 +0100

    hover project working

--- js.js
+++ js.js
@@ -4,6 +4,10 @@ const navLine = [...document.querySelectorAll(".line")];
 const navColor = [...document.querySelectorAll(".nav__text--link")];
 const subpages = [...document.querySelectorAll(".subpages")];
 
+let projectBox = [...document.querySelectorAll(".projectsBox")];
+let imgMobile = [...document.querySelectorAll(".imgM")];
+let imgDesk = [...document.querySelectorAll(".imgD")];
+
 // NAV SCROLL LISTENER
 window.addEventListener("scroll", () => {
   for (let [subpage, className] of subpages.entries()) {
@@ -25,4 +29,20 @@ function navChanges(navData) {
   }
   navColor[navData].classList.add("active__color");
   navLine[navData].classList.add("active__line");
+}
+
+// HOVER PROJECTS
+//MOUSEOVER
+for (let [numImg] of projectBox.entries()) {
+  projectBox[numImg].addEventListener("mouseover", function () {
+    imgMobile[numImg].src = `./landscapes/${numImg + 1}mg.gif`;
+    imgDesk[numImg].src = `./landscapes/${numImg + 1}dg.gif`;
+  });
+}
+//MOUSEOUT
+for (let [numImg] of projectBox.entries()) {
+  projectBox[numImg].addEventListener("mouseout", function () {
+    imgMobile[numImg].src = `./landscapes/${numImg + 1}m.png`;
+    imgDesk[numImg].src = `./landscapes/${numImg + 1}d.png`;
+  });
 }
